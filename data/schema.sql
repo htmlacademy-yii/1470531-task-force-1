fa652e251e9d650937d88cbc4cc769333934a3e3
Alexey Ivanov <4985900@gmail.com>
2021-02-26 11:43:50 +0300

    fix: поправил схему

--- data/schema.sql
+++ data/schema.sql
@@ -7,15 +7,16 @@ CREATE TABLE city
     id         BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     created_on TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     title      TEXT   NOT NULL,
-    lat        DOUBLE NOT NULL,
-    `long`     DOUBLE NOT NULL
+    latitude   DOUBLE NOT NULL,
+    longitude  DOUBLE NOT NULL
 );
 
 CREATE TABLE category
 (
     id         BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     created_on TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    title      TEXT NOT NULL
+    title      TEXT NOT NULL,
+    icon       TEXT NOT NULL
 );
 
 CREATE TABLE specialization
@@ -29,18 +30,25 @@ CREATE TABLE user
 (
     id         BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     created_on TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    last_login TIMESTAMP,
     name       TEXT         NOT NULL,
     email      VARCHAR(250) NOT NULL UNIQUE,
-    password   TEXT         NOT NULL,
+    password   TEXT         NOT NULL
+);
+
+CREATE TABLE profile
+(
+    id         BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    created_on TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    last_login TIMESTAMP,
     birthday   TIMESTAMP,
     about      TEXT,
     phone      VARCHAR(15),
     skype      VARCHAR(250),
     telegram   VARCHAR(250),
     avatar     TEXT,
-    city_id    BIGINT UNSIGNED,
-    FOREIGN KEY (city_id) REFERENCES city (id)
+    user_id    BIGINT UNSIGNED,
+    address    BIGINT UNSIGNED,
+    FOREIGN KEY (user_id) REFERENCES user (id)
         ON DELETE SET NULL
         ON UPDATE SET NULL
 );
@@ -72,19 +80,18 @@ CREATE TABLE photo_of_work
 
 CREATE TABLE task
 (
-    id             BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    created_on     TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    title          TEXT         NOT NULL,
-    description    TEXT         NOT NULL,
-    budget         INT UNSIGNED NOT NULL,
-    execution_date TIMESTAMP    NOT NULL,
-    category_id    BIGINT UNSIGNED,
-    city_id        BIGINT UNSIGNED,
-    owner_id       BIGINT UNSIGNED,
+    id          BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    created_on  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    title       TEXT         NOT NULL,
+    description TEXT         NOT NULL,
+    budget      INT UNSIGNED NOT NULL,
+    expire      TIMESTAMP    NOT NULL,
+    category_id BIGINT UNSIGNED,
+    latitude    DOUBLE       NOT NULL,
+    longitude   DOUBLE       NOT NULL,
+    address     TEXT,
+    owner_id    BIGINT UNSIGNED,
     FOREIGN KEY (category_id) REFERENCES category (id)
-        ON DELETE SET NULL
-        ON UPDATE SET NULL,
-    FOREIGN KEY (city_id) REFERENCES city (id)
         ON DELETE SET NULL
         ON UPDATE SET NULL,
     FOREIGN KEY (owner_id) REFERENCES user (id)
