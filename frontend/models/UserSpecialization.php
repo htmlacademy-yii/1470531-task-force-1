c0270d0e49a787019e7a88776c5577df0c2b49b7
Alexey Ivanov <4985900@gmail.com>
2021-03-02 15:51:17 +0300

    feat: добавил модель

---
+++ frontend/models/UserSpecialization.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace frontend\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "user_specialization".
+ *
+ * @property int $id
+ * @property string|null $created_on
+ * @property int|null $user_id
+ * @property int|null $specialization_id
+ *
+ * @property User $user
+ * @property Specialization $specialization
+ */
+class UserSpecialization extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'user_specialization';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['created_on'], 'safe'],
+            [['user_id', 'specialization_id'], 'integer'],
+            [
+                ['user_id'],
+                'exist',
+                'skipOnError' => true,
+                'targetClass' => User::className(),
+                'targetAttribute' => ['user_id' => 'id']
+            ],
+            [
+                ['specialization_id'],
+                'exist',
+                'skipOnError' => true,
+                'targetClass' => Specialization::className(),
+                'targetAttribute' => ['specialization_id' => 'id']
+            ],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'created_on' => 'Created On',
+            'user_id' => 'User ID',
+            'specialization_id' => 'Specialization ID',
+        ];
+    }
+
+    /**
+     * Gets query for [[User]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getUser()
+    {
+        return $this->hasOne(User::className(), ['id' => 'user_id']);
+    }
+
+    /**
+     * Gets query for [[Specialization]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getSpecialization()
+    {
+        return $this->hasOne(Specialization::className(), ['id' => 'specialization_id']);
+    }
+}
